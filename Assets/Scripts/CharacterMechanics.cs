c70918ebd45e9a5c530ada307c845e06ba2c5a66
ryabchikk <mixail-12-r@mail.ru>
2022-04-14 08:00:22 +0300

    исправил пару вещей

--- Assets/Scripts/CharacterMechanics.cs
+++ Assets/Scripts/CharacterMechanics.cs
@@ -16,31 +16,30 @@ public class CharacterMechanics : MonoBehaviour
     {
         ch_controller = GetComponent<CharacterController>();
     }
-
     void Update()
     {
+        CharacterJump();
         CharacterMove();
     }
     private void CharacterMove()
+    {
+        moveVector = Vector3.zero;
+        moveVector.x = Input.GetAxis("Horizontal") * speed;
+        moveVector.z = Input.GetAxis("Vertical") * speed;
+        Vector3 move = transform.right * moveVector.x + transform.forward * moveVector.z;
+        ch_controller.Move(move * Time.deltaTime);
+    }
+    private void CharacterJump() 
     {
         if (ch_controller.isGrounded && velocity.y < 0) 
         {
             velocity.y = -2f;
         }
-        
-        moveVector = Vector3.zero;
-        moveVector.x = Input.GetAxis("Horizontal") * speed;
-        moveVector.z = Input.GetAxis("Vertical") * speed;
-        
-        Vector3 move = transform.right * moveVector.x + transform.forward * moveVector.z;
-        
-        ch_controller.Move(move * Time.deltaTime);
-        if (Input.GetKeyDown(KeyCode.Space)&& ch_controller.isGrounded) 
+        if (ch_controller.isGrounded && Input.GetKey(KeyCode.Space))
         {
-            velocity.y = Mathf.Sqrt(3f * -2f * gravity);
+            velocity.y = Mathf.Sqrt(jumpPow * -2f * gravity);
         }
         velocity.y += gravity * Time.deltaTime;
-        
         ch_controller.Move(velocity * Time.deltaTime);
     }
 }
