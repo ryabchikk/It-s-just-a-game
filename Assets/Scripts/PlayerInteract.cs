c0119232bec3732dcacf2665c171c08bec894e1e
Lyrai <x355x02c@live.com>
2022-04-15 00:15:13 +0300

    Fixed pickup

--- Assets/Scripts/PlayerInteract.cs
+++ Assets/Scripts/PlayerInteract.cs
@@ -9,15 +9,23 @@ public class PlayerInteract : MonoBehaviour
     [SerializeField] private float interactDistance;
     [SerializeField] private GameObject player;
     [SerializeField] private new Transform camera;
+    private IInteractable _holdedObject;
     
     public void Interact()
     {
         var mask = 1 << 6;
         Debug.DrawLine(transform.position, transform.forward * interactDistance, Color.red);
+        if (_holdedObject is Pickable _)
+        {
+            _holdedObject.Interact(this);
+            _holdedObject = null;
+            return;
+        }
         if (!Physics.Raycast(transform.position, camera.forward, out var hit, interactDistance, mask)) 
             return;
         
         var interactable = hit.collider.gameObject.GetComponent<IInteractable>();
+        _holdedObject = interactable;
 
         interactable?.Interact(this);
     }
