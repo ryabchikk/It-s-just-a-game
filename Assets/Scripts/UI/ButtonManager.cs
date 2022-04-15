917dc442a0ce2fd94a92848f07765317086a60f4
ryabchikk <mixail-12-r@mail.ru>
2022-04-15 17:07:01 +0300

    Добавил сцену главного меню, добавил UI скрипты

---
+++ Assets/Scripts/UI/ButtonManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Linq;
+
+public class ButtonManager : MonoBehaviour
+{
+
+    public void LoadLevel(int level)
+    {
+        SceneManager.LoadScene(level);
+        Time.timeScale = 1;
+    }
+    public void OpenPanel(GameObject obj)
+    {
+        obj.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void ClosePanel(GameObject obj)
+    {
+        obj.SetActive(false);
+        Time.timeScale = 1;
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
+
