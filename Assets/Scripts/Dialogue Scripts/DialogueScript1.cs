7cc695a747c27473c4c59f26d6f2aed1c0a35ae3
i am nena <58001221+nenapisannoe@users.noreply.github.com>
2022-04-16 20:18:17 +0300

    added rolling dialogue

---
+++ Assets/Scripts/Dialogue Scripts/DialogueScript1.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueScript1 : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+    [SerializeField] Text button1text;
+    [SerializeField] Text button2text;
+
+    [SerializeField] GameObject button1;
+    [SerializeField] GameObject button2;
+
+
+    State state;
+
+	// Use this for initialization
+	void Start()
+    {
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+        SetScene();
+	}
+	
+	// Update is called once per frame
+    public void SetScene()
+    {
+        button1text.text = state.Buttons[0];
+        button2text.text = state.Buttons[1];
+    }
+    public void FirstChoice()
+    { 
+        var nextStates = state.GetNextStates();
+        state = nextStates[0];
+        textComponent.text = state.GetStateStory();
+        Debug.Log("Pressed");
+        SetScene();
+    }
+    public void SecondChoice()
+    {
+        var nextStates = state.GetNextStates();
+        state = nextStates[1];
+        textComponent.text = state.GetStateStory();
+        SetScene();
+    }
+
+}
