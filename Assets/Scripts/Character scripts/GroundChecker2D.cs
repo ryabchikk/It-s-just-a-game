84dfe505d841982e89eaa80ce44db82640b3c0c0
ryabchikk <mixail-12-r@mail.ru>
2022-04-16 18:42:01 +0300

    Поправил 2D управление

---
+++ Assets/Scripts/Character scripts/GroundChecker2D.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundChecker2D : MonoBehaviour
+{
+    [SerializeField] Movement2D player;
+    [SerializeField] Collider playerCollider;
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other != null)
+        {
+            player.SetOnGround(false);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other != null && other != playerCollider)
+        {
+            player.SetOnGround(true);
+        }
+    }
+}
