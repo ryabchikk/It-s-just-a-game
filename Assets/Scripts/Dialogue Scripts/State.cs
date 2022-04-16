1e091eb0dd4dc43e4ec6cf187e01eb7ff9c37c96
i am nena <58001221+nenapisannoe@users.noreply.github.com>
2022-04-16 19:32:00 +0300

    добавила сцену для новеллы и диалогов

---
+++ Assets/Scripts/Dialogue Scripts/State.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject {
+
+    [TextArea(14,10)] [SerializeField] string storyText;
+    [SerializeField] public string[] Buttons;
+    [SerializeField] State[] nextStates;
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+
+    public State[] GetNextStates()
+    {
+        return nextStates;
+    }
+
+}
